073dbd4dab9d96ff1afb0b33cbc079837f12a78c
Brian Fan <kuomu.fan@yahoo.com>
2017-06-21 14:55:48 -0700

    fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,6 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
 
-  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth, private userService: UserService) {
-    this.items = db.list('/items');
-    this.user = afAuth.authState;
-  }
-
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(response => {
-      console.log(response);
-      // this.userName = response.user.displayName;
-
-    });
-  }
-
-  logout() {
-    this.afAuth.auth.signOut();
-
   ngOnInit() {
   }
 }
