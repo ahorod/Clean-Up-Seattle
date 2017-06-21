d330729c302940cabf2e515bf299e6c97063ebc4
AH and KB <ah+kb@>
2017-06-21 14:56:49 -0700

    just to merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,8 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
 
-  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth, private userService: UserService) {
-    this.items = db.list('/items');
-    this.user = afAuth.authState;
-  }
+  constructor() {}
 
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(response => {
-      console.log(response);
-      // this.userName = response.user.displayName;
-
-    });
-  }
-
-  logout() {
-    this.afAuth.auth.signOut();
 
   ngOnInit() {
   }
